f5769329e384368a8de6a6605ccd6d5ab58ee309
zolza <zolza@DESKTOP-GAMVOHS>
2021-05-11 09:08:49 -0500

    created Appointment Delete  end point

--- AppReservation/src/main/java/com/example/demo/service/AppointmentService.java
+++ AppReservation/src/main/java/com/example/demo/service/AppointmentService.java
@@ -1,5 +1,20 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.AppointmentRepository;
+
+@Service
 public class AppointmentService implements IAppointmentService{
 
+	@Autowired
+	AppointmentRepository repo;
+	@Override
+	public void deleteAppointment(String id) {
+		// TODO Auto-generated method stub
+		repo.deleteById(id);
+	}
+
+	
 }
--- AppReservation/src/main/java/com/example/demo/service/IAppointmentService.java
+++ AppReservation/src/main/java/com/example/demo/service/IAppointmentService.java
@@ -2,4 +2,5 @@ package com.example.demo.service;
 
 public interface IAppointmentService {
 
+	public void deleteAppointment(String id);
 }
